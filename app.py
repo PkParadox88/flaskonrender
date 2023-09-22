eb87a663d5abf348121ce50b2748ad5f4543eba0
PRAHLAD KUMAR <prahladkumar3741@gmail.com>
2023-09-22 14:22:12 +0545

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 import os
 import requests
+import time
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 # Replace with your Telegram bot token
 BOT_TOKEN = "6526048633:AAHWo8UD4h7R64LV_R7U4YcXg-fU_7WiHLY"
-# pCloud info
-username = "prahladkumar3741@gmail.com"
-password = "Qs%eQe@/DAHZCe4"
+pcloud_auth = "MeF2iVZXtk8Zo8FNiGnn4mJ8rgTygUB0wXhXNPPV"
 
 # Define the base URL for the Telegram Bot API
 BASE_URL = f"https://api.telegram.org/bot{BOT_TOKEN}"
+
 
 # Function to send a message
 def send_message(chat_id, text):
@@ -26,6 +26,16 @@ def pcloud_notification(username, password):
     print("hello bro")
 
 
+def list_items():
+    session = requests.Session()
+    folderid = '6616465256'
+    adata = {'auth': 'MeF2iVZXtk8Zo8FNiGnn4mJ8rgTygUB0wXhXNPPV', 'folderid': folderid}
+    post = session.post('http://eapi.pcloud.com/listfolder?', data=adata)
+    data = post.json()['metadata']['contents']
+    print(data)
+    return data
+    
+    
 @app.route('/')
 def home():
     return "Welcome to the Max Maven Bot!"
@@ -63,6 +73,9 @@ def handle_commands(chat_id, command):
     elif command == "/pcloud":
         pcloud_notification(username, password)
         send_message(chat_id, "Here are the recent changes in Pcloud :")
+    elif command == "/items":
+        items = list_items()
+        send_message(chat_id, items)
     else:
         send_message(chat_id, "I'm not sure what you mean. Use /help to see available commands.")
 
@@ -74,4 +87,3 @@ def hello_world():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
-
